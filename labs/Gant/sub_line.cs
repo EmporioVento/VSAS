e16f78e3c7441660b3cb0f6f90df5f71f49b5d12
AnnaSiur <anna.kiev92@gmail.com>
2015-12-01 23:23:03 +0200

    Revert "Revert "Revert "Revert "All ADD to Repo maybe""""
    This reverts commit 05ca684eda15ffd250262288fad37ad3a53496af.

---
+++ labs/Gant/sub_line.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using labs.Graph;
+using labs.GraphCS;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace labs.Gant
+{    
+    public class sub_line
+    {
+        private Label label_horizontal;
+        private Shape SubLineHorizontal;
+        private DestinationAlgorithms _dest_alg;
+        private horizontal_line _horiz_line;
+        public int from_id;
+        public int to_id;
+        public double posY;
+
+        public sub_line(DestinationAlgorithms _dest_alg, horizontal_line _horiz_line, double startY, int from_id, int to_id)
+        {
+            this._dest_alg = _dest_alg;
+            this._horiz_line = _horiz_line;
+            this.from_id = from_id;
+            this.to_id = to_id;
+
+            SubLineHorizontal = new Line();
+            SubLineHorizontal.Stroke = Brushes.Blue;
+            SubLineHorizontal.StrokeThickness = 0.2;
+            ((Line)SubLineHorizontal).X1 = 50;
+            ((Line)SubLineHorizontal).Y1 = startY;
+            ((Line)SubLineHorizontal).X2 = 9950;
+            ((Line)SubLineHorizontal).Y2 = startY;
+            posY = startY;
+            if (_horiz_line.SubLinesHorizontal == null)
+                _horiz_line.SubLinesHorizontal = new List<sub_line>();
+
+            _horiz_line.SubLinesHorizontal.Add(this);
+            label_horizontal = new Label();
+            label_horizontal.HorizontalContentAlignment = HorizontalAlignment.Center;
+            label_horizontal.HorizontalContentAlignment = HorizontalAlignment.Center;
+            label_horizontal.FontSize = 8;
+            label_horizontal.Content = from_id + "-" + to_id;
+            label_horizontal.Width = 30;
+
+            _dest_alg.GantCanvas.Children.Add(SubLineHorizontal);
+            _dest_alg.GantCanvas.Children.Add(label_horizontal);
+            Canvas.SetZIndex(SubLineHorizontal, 0);
+            Canvas.SetZIndex(label_horizontal, 2);
+
+            Canvas.SetLeft(label_horizontal, 25);
+            Canvas.SetTop(label_horizontal, startY - 12);           
+        }
+    }
+}
---
+++ labs/GraphCS/edge_view.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace labs.GraphCS
+{
+    [Serializable]
+    public class edge_view
+    {
+        private Shape Line;
+        private List<Shape> Lines;
+        private bool isLine;
+        private graph_viewCS _graph;
+        private node_view from_node;
+        private node_view to_node;
+
+        Thickness th = new Thickness(1.1);
+
+        public edge_view(graph_viewCS _graph, node_view from_node, node_view to_node)
+        {
+            this._graph = _graph;
+            if (from_node != to_node)
+                isLine = true;
+            else
+                isLine = false;
+            if (isLine)
+                Line = new Line();
+            else
+                Line = new Ellipse();
+
+            Line.MouseLeftButtonDown += new MouseButtonEventHandler(Line_MouseLeftButtonDown);
+            Line.Stroke = Brushes.Black;
+            Line.StrokeThickness = 1;
+            if (isLine)
+            {
+                ((Line)Line).X1 = 0;
+                ((Line)Line).Y1 = 0;              
+            }
+            else
+            {
+                ((Ellipse)Line).Width = 50;
+                ((Ellipse)Line).Height = 50;
+            }
+            if (Lines == null)
+            {
+                Lines = new List<Shape>();
+                Lines.Add(Line);
+            }
+            _graph.GRCanvas.Children.Add(Line);
+            Canvas.SetZIndex(Line, 0);
+            this.from_node = from_node;
+            this.to_node = to_node;
+            to_node.pointPositionChange += new graph_viewCS.PointPositionChanged(OnPointPositionChanged);
+            from_node.pointPositionChange += new graph_viewCS.PointPositionChanged(OnPointPositionChanged);
+            OnPointPositionChanged(to_node);
+            if (isLine)
+                OnPointPositionChanged(from_node);
+        }
+
+        public bool isValid { get; set; }
+
+        public node_view From
+        {
+            get { return from_node; }
+        }
+
+        public node_view To
+        {
+            get { return to_node; }
+        }
+
+        public bool IsLine
+        {
+            get { return isLine; }
+        }
+
+        public List<Shape> Edge
+        {
+            get { return Lines; }
+        }
+
+        private void Line_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_graph.IsEdgeDelete)
+            {
+                _graph.DeleteEdge(this);
+                _graph.EndDeleteEdge();
+            }
+        }
+
+        public void OnPointPositionChanged(node_view top)
+        {
+            if (isLine)
+            {
+                if (top == from_node)
+                {
+                    Canvas.SetLeft(Line, from_node.RELPos.X + from_node.GRNode.Width / 2);
+                    Canvas.SetTop(Line, from_node.RELPos.Y + graphNode.Radius / 2);
+                }
+                ((Line)Line).X2 = to_node.RELPos.X - from_node.RELPos.X;
+                ((Line)Line).Y2 = to_node.RELPos.Y - from_node.RELPos.Y;
+            }
+            else
+            {
+                Canvas.SetLeft(Line, from_node.RELPos.X);
+                Canvas.SetTop(Line, from_node.RELPos.Y - 35);
+            }
+        }
+    }
+}
